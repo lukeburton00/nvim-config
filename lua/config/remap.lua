6a10886ba23f9c191c5a4232405522ffd77ba5a6
Luke Burton <lukesburton.com@gmail.com>
2024-08-30 17:31:05 -0600

    initial commit

---
+++ lua/config/remap.lua
@@ -0,0 +1,33 @@
+vim.g.mapleader = ' '
+vim.keymap.set("n", "<leader><space>", vim.cmd.Ex)
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+vim.api.nvim_create_autocmd('filetype', {
+  pattern = 'netrw',
+  desc = 'Better mappings for netrw',
+  callback = function()
+    local bind = function(lhs, rhs)
+      vim.keymap.set('n', lhs, rhs, {remap = true, buffer = true})
+    end 
+
+    -- edit new file
+    bind('n', '%')
+
+    -- rename file
+    bind('r', 'R')
+  end
+})
