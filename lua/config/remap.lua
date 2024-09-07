062f95ba36458a8e4fca391f42a95f416c027598
Luke Burton <lukesburton19@gmail.com>
2024-09-07 12:00:18 -0600

    remove quit and tmux-sessionizer from remap.lua

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,6 +1,5 @@
 vim.g.mapleader = ' '
 
-vim.keymap.set("n", "<leader>q", vim.cmd.q)
 vim.keymap.set("n", "<C-`>", vim.cmd.terminal)
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -15,5 +14,3 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 vim.keymap.set("n", "Q", "<nop>")
-
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
