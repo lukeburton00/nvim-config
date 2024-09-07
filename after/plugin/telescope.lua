865f40497d3d3db4d351f5c4bd943866e361ebd9
Luke Burton <lukesburton19@gmail.com>
2024-09-07 13:41:49 -0600

    remove incompatible old telescope mappings and add new

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,21 +1,8 @@
 require('telescope').setup {}
 
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-
-vim.keymap.set('n', '<leader>pws', function()
-    local word = vim.fn.expand("<cword>")
-    builtin.grep_string({ search = word })
-end)
-
-vim.keymap.set('n', '<leader>pWs', function()
-    local word = vim.fn.expand("<cWORD>")
-    builtin.grep_string({ search = word })
-end)
-
-vim.keymap.set('n', '<leader>s', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+vim.keymap.set('n', '<C-p>', builtin.lsp_document_symbols)
 
 vim.keymap.set('n', '<leader>hh', builtin.help_tags, {})
