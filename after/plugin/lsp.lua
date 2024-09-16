99bcb33fa70c71b24cbd1af0d8ec76d1e31fc548
Luke Burton <lukesburton19@gmail.com>
2024-09-16 15:46:29 -0600

    consolidate cmp and lsp into one set of configs (lsp.lua)

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,3 +1,23 @@
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('user_lsp_attach', {clear = true}),
+  callback = function(event)
+    local opts = {buffer = event.buf}
+
+    vim.keymap.set('n', '<leader>gd', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', '<leader>h', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', '<leader>ws', function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set('n', '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set('n', '<leader>rr', function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+  end,
+})
+
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 local lspconfig = require("lspconfig")
 
 require("mason").setup()
@@ -11,6 +31,53 @@ lspconfig.lua_ls.setup {}
 
 require("mason-lspconfig").setup_handlers {
     function(server)
+        capabilities = lsp_capabilities
         lspconfig[server].setup({})
     end
+}
+
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+luasnip.config.setup {}
+
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end
+    },
+
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+    },
+
+    mapping = cmp.mapping.preset.insert {
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete {},
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+            end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+        else
+            fallback()
+        end
+        end, { 'i', 's' }),
+    },
 }
