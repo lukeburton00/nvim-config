26f924addc85e420e93af8864443d9383ea11232
Luke Burton <lukeburton@Lukes-MacBook-Pro.local>
2024-09-25 21:38:18 -0600

    add keybind to return from goto definition command

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = ' '
 
 vim.keymap.set("n", "<C-`>", vim.cmd.terminal)
 
+vim.keymap.set("n", "<leader>b", "<C-t>")
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
