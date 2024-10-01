18a2903a938daee70d5256bc96d75ff8055ac0ab
Luke Burton <=>
2024-10-01 13:55:54 -0600

    add correct grep string config to telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,4 +5,8 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.lsp_document_symbols)
 
+vim.keymap.set('n', '<leader>gs', function()  
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+
 vim.keymap.set('n', '<leader>hh', builtin.help_tags, {})
