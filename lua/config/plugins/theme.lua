844ecf56beb9d54738d46e64dbf5dfa0505a5f41
Luke Burton <lukesburton19@gmail.com>
2024-10-03 21:09:23 -0600

    update theme.lua

--- lua/config/plugins/theme.lua
+++ lua/config/plugins/theme.lua
@@ -1,6 +1,7 @@
 return {
   'projekt0n/github-nvim-theme',
-  priority = 1000, -- make sure to load this before all the other start plugins
+  lazy = false,
+  priority = 1000,
   config = function()
     require('github-theme').setup({
       -- ...
