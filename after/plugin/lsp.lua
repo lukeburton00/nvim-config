57217be6b0ce6a6d712d08d71f8d0392d9143753
Luke Burton <lukesburton19@gmail.com>
2024-10-03 21:07:36 -0600

    update signature help key to use leader h

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -12,7 +12,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set('n', '<leader>rr', function() vim.lsp.buf.references() end, opts)
     vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set('n', '<leader>hs', function() vim.lsp.buf.signature_help() end, opts)
   end,
 })
 
