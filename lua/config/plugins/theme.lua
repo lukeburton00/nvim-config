620dceb7d6ad34f7cfbd1d5585017c94f14314bf
Luke Burton <=>
2024-10-12 21:40:57 -0600

    update theme to catppuccin

--- lua/config/plugins/theme.lua
+++ lua/config/plugins/theme.lua
@@ -1,12 +1,10 @@
 return {
-  'projekt0n/github-nvim-theme',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require('github-theme').setup({
-      -- ...
-    })
+    "catppuccin/nvim", name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require('catppuccin').setup()
 
-    vim.cmd('colorscheme github_dark')
-  end,
+        vim.cmd('colorscheme catppuccin-mocha')
+    end,
 }
