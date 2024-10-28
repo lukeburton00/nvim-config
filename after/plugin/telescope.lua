6868f6ccfcf17c25364780896916c80901340355
Luke Burton <=>
2024-10-28 11:02:54 -0600

    use leader s for live grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,6 +5,6 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.lsp_document_symbols)
 
-vim.keymap.set('n', '<leader>gs', builtin.live_grep)
+vim.keymap.set('n', 's', builtin.live_grep)
 
 vim.keymap.set('n', '<leader>hh', builtin.help_tags, {})
