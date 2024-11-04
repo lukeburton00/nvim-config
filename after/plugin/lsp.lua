2158be1ec763bfc8ee2b2aafbbc25094f900e9cf
Luke Burton <=>
2024-11-04 10:47:32 -0700

    update lsp references binding

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,7 +10,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
     vim.keymap.set('n', '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set('n', '<leader>rr', function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set('n', '<leader>rf', function() vim.lsp.buf.references() end, opts)
     vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set('n', '<leader>hs', function() vim.lsp.buf.signature_help() end, opts)
   end,
