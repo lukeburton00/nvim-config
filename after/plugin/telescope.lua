a1cd8e56cfdaa3a29bb1e590766b633b9cabbc03
Luke Burton <=>
2024-11-04 10:47:55 -0700

    update telescope find files to include gitignored files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,7 +2,7 @@ require('telescope').setup {}
 
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>f', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>")
+vim.keymap.set('n', '<leader>f', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git', '-u' }})<cr>")
 vim.keymap.set('n', '<C-p>', builtin.lsp_document_symbols)
 
 vim.keymap.set('n', 's', builtin.live_grep)
