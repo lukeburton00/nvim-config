a61077d91a25750fc0108ae4dbbfdd86bf170d84
Luke Burton <=>
2024-11-18 12:11:56 -0700

    add themery

---
+++ after/plugin/themery.lua
@@ -0,0 +1,12 @@
+require("themery").setup({
+    themes = {
+        {
+            name = "Mocha",
+            colorscheme = "catppuccin-mocha",
+        },
+        {
+            name = "Rose Pine",
+            colorscheme = "rose-pine"
+        }
+    }
+})
---
+++ lua/config/plugins/themes.lua
@@ -0,0 +1,6 @@
+return {
+    {"catppuccin/nvim", name = "catppuccin"},
+    {"rose-pine/neovim", name = "rose-pine"},
+    lazy = false,
+    priority = 1000,
+}
