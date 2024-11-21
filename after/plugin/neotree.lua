d5f8aaf02e9d127225701623621daa269812d212
Luke Burton <=>
2024-11-20 21:53:12 -0700

    update neotree to open current file in tree

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,10 +1,11 @@
 require('neo-tree').setup {
     filesystem = {
         hijack_netrw_behavior = "open_current",
+        follow_current_file = { enabled = true },
         filtered_items = {
             visible = true,
         }
     },
 }
 
-vim.keymap.set("n", "<leader><space>", '<CMD>Neotree current<CR>')
+vim.keymap.set("n", "<leader><space>", '<CMD>Neotree current reveal<CR>')
