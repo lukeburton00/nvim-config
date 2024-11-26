de43175ba3191a888f585b4fd8d17fe8e2a252e6
Luke Burton <=>
2024-11-26 16:59:10 -0700

    add new themes

--- after/plugin/themery.lua
+++ after/plugin/themery.lua
@@ -7,7 +7,47 @@ require("themery").setup({
         {
             name = "Rose Pine",
             colorscheme = "rose-pine"
-        }
+        },
+        {
+            name = "Darker",
+            colorscheme = "material",
+            before = [[ 
+                vim.g.material_style = 'darker'
+            ]]
+        },
+        {
+            name = "Oceanic",
+            colorscheme = "material",
+            before = [[ 
+                vim.g.material_style = 'oceanic'
+            ]]
+        },
+        {
+            name = "Pale Night",
+            colorscheme = "material",
+            before = [[ 
+                vim.g.material_style = 'palenight'
+            ]]
+        },
+        {
+            name = "Deep Ocean",
+            colorscheme = "material",
+            before = [[ 
+                vim.g.material_style = 'deep ocean'
+            ]]
+        },
+        {
+            name = "Dragon",
+            colorscheme = "kanagawa-dragon",
+        },
+        {
+            name = "Wave",
+            colorscheme = "kanagawa-wave",
+        },
+        {
+            name = "Lackluster",
+            colorscheme = "lackluster",
+        },
     }
 })
 
--- lua/config/plugins/themes.lua
+++ lua/config/plugins/themes.lua
@@ -1,6 +1,9 @@
 return {
     {"catppuccin/nvim", name = "catppuccin"},
     {"rose-pine/neovim", name = "rose-pine"},
+    {"marko-cerovac/material.nvim", name = "material"},
+    {"rebelot/kanagawa.nvim", name = "kanagawa"},
+    {"slugbyte/lackluster.nvim", name = "lackluster"},
     lazy = false,
     priority = 1000,
 }
