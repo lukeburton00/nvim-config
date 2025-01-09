8b34a9df34a7a7efe456c874d777f5ed864de841
Luke Burton <lukesburton19@gmail.com>
2025-01-09 00:06:47 -0700

    remove zombie code

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -17,7 +17,6 @@ require('telescope').setup {
 require('telescope').load_extension('fzf')
 local builtin = require('telescope.builtin')
 
--- vim.keymap.set('n', '<leader>f', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git', '-u' }})<cr>")
 vim.keymap.set('n', '<leader>f', builtin.find_files)
 vim.keymap.set('n', '<C-p>', builtin.lsp_document_symbols)
 
