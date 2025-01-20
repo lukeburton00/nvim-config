37a2b63d3fac1089ccc0391d44a7693c537feeb3
Luke Burton <lukesburton19@gmail.com>
2025-01-19 18:02:13 -0700

    enable ouroboros only for c and cpp files

--- lua/config/plugins/ouroboros.lua
+++ lua/config/plugins/ouroboros.lua
@@ -3,6 +3,7 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim"
     },
+    ft = {'c', 'cpp'},
 
     config = function ()
         vim.keymap.set('n', '<leader>o', '<cmd>:Ouroboros<CR>', {})
