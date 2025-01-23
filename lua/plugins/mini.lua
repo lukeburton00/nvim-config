9e11fea3cdfedd9a42582e66758666d4db61325a
Luke Burton <lukesburton19@gmail.com>
2025-01-23 13:12:01 -0700

    remove bbq and lualine, add more mini modules

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,8 +3,18 @@ return {
     version = '*',
 
     config = function ()
+        -- Modules
         require("mini.ai").setup()
         require("mini.surround").setup()
+        require("mini.move").setup()
+
         require("mini.pairs").setup()
+
+        require("mini.icons").setup()
+        require("mini.statusline").setup()
+        require("mini.cursorword").setup()
+
+        -- Config
+        MiniIcons.mock_nvim_web_devicons()
     end
 }
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -4,7 +4,6 @@ return {
     lazy = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
     },
 
