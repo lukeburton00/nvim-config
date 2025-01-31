4fa8fcc06cca67a85011b6a8a832b09629fcd904
Luke Burton <lukesburton19@gmail.com>
2025-01-30 22:36:32 -0700

    switch from avante to code companion

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,20 @@
+return {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+
+    config = function ()
+        require("codecompanion").setup({
+            strategies = {
+                chat = {
+                    adapter = "openai"
+                },
+                inline = {
+                    adapter = "openai"
+                }
+            }
+        })
+    end
+}
