27cdb7b3575ba3d80399cbecd18888abc56cbc0d
Luke Burton <lukesburton19@gmail.com>
2025-02-09 23:54:39 -0700

    tab for completions again

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,6 +8,8 @@ return {
     opts = {
         keymap = {
             preset = 'enter',
+            ["<Tab>"]   = { "select_next", "snippet_forward", "fallback" },
+            ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
         },
 
         appearance = {
