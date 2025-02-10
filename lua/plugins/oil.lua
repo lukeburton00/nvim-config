dcc7bc3117928f690715bc4bdbb3f609b641b8bc
Luke Burton <=>
2025-02-10 16:25:00 -0700

    oil needs to show hidden files

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,7 +5,11 @@ return {
     lazy = false,
 
     config = function ()
-        require("oil").setup()
+        require("oil").setup {
+            view_options = {
+                show_hidden = true
+            }
+        }
         vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
     end
